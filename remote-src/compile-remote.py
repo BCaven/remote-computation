936b3e6191a1bdd3c78fd0ec80fd6fd74ef35a5c
Blake <jcaven@nd.edu>
2023-10-31 10:39:02 -0400

    shell of remote compiler

---
+++ remote-src/compile-remote.py
@@ -0,0 +1,18 @@
+#! usr/bin/env python3
+"""
+    Wait for signal from the client then run the command of their choice
+    Output of that command gets stored in the completed-queue/
+
+    PROJECT_DIR is set by the user
+    PROJECT_DIR defaults to default-project/
+"""
+
+
+def main() -> int:
+    """
+        Main driver for the remote signal handler
+    """
+    return 0
+
+if __name__ == "__main__":
+    main()
